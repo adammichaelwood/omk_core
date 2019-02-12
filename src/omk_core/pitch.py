8fbcc36de6eaa41814173393d31e4c4464e3561c
Adam Wood <adam.michael.wood@gmail.com>
2019-02-12 09:19:16 -0800

    organized Pitch.py

--- src/omk_core/pitch.py
+++ src/omk_core/pitch.py
@@ -1,38 +1,95 @@
-"""Pitch is constructor for a TonalVector.
-
->>> Pitch('gsharp')
-TonalVector((4, 8))
-
-
->>> Pitch('Bb')
-TonalVector((6, 10))
-
->>> Pitch('C#')
-TonalVector((0, 1))
-
->>> Pitch('eis')
-TonalVector((2, 3))
-
->>> Pitch('Db3')
-TonalVector((1, 1, 3))
-
->>> C0 = Pitch('c0')
->>> Pitch("a'", C0)
-TonalVector((5, 9, 0))
-
-"""
-
 import re
 
 import tonal_arithmetic as ta
 import tonal_vector as tv
 
 
+def Pitch(nstr, octave_context=None):
+    """Returns a TonalVector from a human readable note string.
 
-note_parser = re.compile('([a-gA-G])\s*([^-\d\s\'\,]*)\s*(-?[0-9\'\,]*)')
+    Parameters
+    ----------
 
-def octave_reader(note_tuple, octave_str, octave_context=None):
-    """Returns the correct octave, given a re.match object for a pitch and, optionally, an octave context.
+    nstr : str
+        A human readable string representing a note, with or without an octave designation.
+        Accepts:
+
+        - letter names a-b (case insensitive)
+
+        - modifiers/accidentals
+        
+          - spelled out (sharp, flat)
+          - ascii (#, b)
+          - unicode (♯, ♭)
+          - ly (es, is)
+
+        - octave designations
+
+          - int
+          - ly (''' ,,,)
+
+        
+        Examples: cflat, D#, Ab3, bes'
+
+    octave_context : tonal object or 'c4'
+        If nstr is a lilypond representation using relative octaves,
+        octave_context is the preceding note.
+
+        If nstr is an int,
+        octave_context may be the string c4,
+        indicating that the nstr is written using c4 octave designations.
+        # Need to test this.
+
+    
+    >>> Pitch('gsharp')
+    TonalVector((4, 8))
+
+    >>> Pitch('Bb')
+    TonalVector((6, 10))
+
+    >>> Pitch('C#')
+    TonalVector((0, 1))
+
+    >>> Pitch('eis')
+    TonalVector((2, 3))
+
+    >>> Pitch('Db3')
+    TonalVector((1, 1, 3))
+
+    >>> C0 = Pitch('c0')
+    >>> Pitch("a'", C0)
+    TonalVector((5, 9, 0))
+
+    """
+    return tv.TonalVector(pitch_to_tuple(nstr, octave_context))
+    
+
+def pitch_to_tuple(nstr, octave_context=None):
+    note_re = _note_parser.fullmatch(nstr)
+    n = note_re[1].lower()
+    m = note_re[2].lower() or ''
+    o_str = note_re[3] or ''
+    
+    s = [x for x in tv.MS if x.ln == n][0]
+    
+    d = s.d
+    
+    m_key = [key for key,strs in tv.AC.items() if m in strs.values()][0]
+    
+    c = s.c + m_key
+    
+    o = _octave_reader((d,c), o_str, octave_context)
+
+    if o is not None:
+        return (d, c, o)
+    return (d, c)
+
+
+_note_parser = re.compile('([a-gA-G])\s*([^-\d\s\'\,]*)\s*(-?[0-9\'\,]*)')
+
+
+def _octave_reader(note_tuple, octave_str, octave_context=None):
+    """Returns the correct octave, given an re.match object for a pitch and, optionally, an octave context.
     """
 
     """
@@ -47,7 +104,10 @@ def octave_reader(note_tuple, octave_str, octave_context=None):
         pass
     else:
         if octave_context is not None:
-            raise TypeError("An integer octave designation does not need an octave context.")
+            if octave_context.lower() != 'c4':
+                raise TypeError("An integer octave designation does not need an octave context.")
+            else: # context is c4
+                return octave_int - 4
         return octave_int
 
     """
@@ -89,39 +149,6 @@ def octave_reader(note_tuple, octave_str, octave_context=None):
 
 
 
-
-
-
-
-
-
-
-
-
-
-def pitch_to_tuple(nstr, octave_context=None):
-    note_re = note_parser.fullmatch(nstr)
-    n = note_re[1].lower()
-    m = note_re[2].lower() or ''
-    o_str = note_re[3] or ''
-    
-    s = [x for x in tv.MS if x.ln == n][0]
-    
-    d = s.d
-    
-    m_key = [key for key,strs in tv.AC.items() if m in strs.values()][0]
-    
-    c = s.c + m_key
-    
-    o = octave_reader((d,c), o_str, octave_context)
-
-    if o is not None:
-        return (d, c, o)
-    return (d, c)
-
-def Pitch(nstr, octave_context=None):
-    return tv.TonalVector(pitch_to_tuple(nstr, octave_context))
-    
 
 if __name__ == "__main__":
     import doctest
